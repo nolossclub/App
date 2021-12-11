8e1db5dfb58a9f0fe751eef230fca034b7d38fcf
nolossclub <95616694+nolossclub@users.noreply.github.com>
2021-12-11 22:55:32 +0000

    app

---
+++ src/components/CallToAction.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="pt-16 pb-20 bg-blueGray-50">
+    <div class="container px-4 mx-auto">
+      <div class="w-full mb-16">
+        <h2 class="title text-center text-4xl md:text-4xl font-semibold font-heading">
+          <span>What makes us </span>
+          <span class="text-blue-600">Unique</span>
+        </h2>
+      </div>
+      <div class="flex flex-wrap -mx-3 -mb-6">
+        <div class="w-full md:w-1/2 lg:w-1/4 px-3 mb-6">
+          <div class="pt-8 px-6 pb-6 bg-white text-center rounded shadow">
+            <img src="../assets/binance.png" class="w-32 mx-auto mb-8" />
+            <h3 class="text-xl mb-2 font-semibold font-heading">
+              Uses binance smart chain
+            </h3>
+            <p class="text-md text-blueGray-400">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+        <div class="w-full md:w-1/2 lg:w-1/4 px-3 mb-6">
+          <div class="pt-8 px-6 pb-6 bg-white text-center rounded shadow">
+            <img src="../assets/reward.png" class="w-32 mx-auto mb-8" />
+            <h3 class="text-xl mb-2 font-semibold font-heading">Collect rewards</h3>
+            <p class="text-md text-blueGray-400">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+        <div class="w-full md:w-1/2 lg:w-1/4 px-3 mb-6">
+          <div class="pt-8 px-6 pb-6 bg-white text-center rounded shadow">
+            <img src="../assets/nft.png" class="w-32 mx-auto mb-8" />
+            <h3 class="text-xl mb-2 font-semibold font-heading">NFT ticket</h3>
+            <p class="text-md text-blueGray-400">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+        <div class="w-full md:w-1/2 lg:w-1/4 px-3 mb-6">
+          <div class="pt-8 px-6 pb-6 bg-white text-center rounded shadow">
+            <img src="../assets/share.png" class="w-32 mx-auto mb-8" />
+            <h3 class="text-xl mb-2 font-semibold font-heading">Share money</h3>
+            <p class="text-md text-blueGray-400">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="py-16">
+    <div class="container px-4 mx-auto">
+      <div class="flex flex-col lg:flex-row items-center lg:justify-between">
+        <p class="text-lg text-blueGray-400">© 2021. All rights reserved.</p>
+        <div class="order-first lg:order-last -mx-2 mb-4 lg:mb-0">
+          <a class="inline-block mx-2 px-1 w-10" href="#">
+            <img src="../assets/social/discord.png" alt="" />
+          </a>
+          <a class="inline-block mx-2 px-1 w-10" href="#">
+            <img src="../assets/social/medium.png" alt="" />
+          </a>
+          <a class="inline-block mx-2 px-1 w-10" href="#">
+            <img src="../assets/social/telegram.png" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
